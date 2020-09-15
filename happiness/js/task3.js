3694cba077fb3676810bb356827c8212ea443644
LISAIXORA <okalaumahappiness@gmail.com>
2020-09-14 22:30:33 -0600

    completed task3

---
+++ happiness/js/task3.js
@@ -0,0 +1,10 @@
+document.getElementById('respbar').addEventListener('click',function () {
+    document.getElementById('upaside3').style.display="none";
+document.getElementById('upaside2').style.display="inline-block";
+document.getElementById('topnav').style.height='165px';
+});
+document.getElementById('respbar1').addEventListener('click', function() {
+    document.getElementById('upaside2').style.display="none";
+    document.getElementById('upaside3').style.display="inline-block";
+    document.getElementById('topnav').style.height='40px';
+});

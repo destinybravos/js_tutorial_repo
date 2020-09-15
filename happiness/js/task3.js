f32e0f5a199f0bb58588e6e498cb7b2a1b523acc
LISAIXORA <okalaumahappiness@gmail.com>
2020-09-15 16:10:29 -0600

    Finish Task 3

--- happiness/js/task3.js
+++ happiness/js/task3.js
@@ -1,4 +1,5 @@
 document.getElementById('respbar').addEventListener('click',function () {
+    document.getElementById('upaside1').style.padding="36px 0 5px 8px";
     document.getElementById('upaside3').style.display="none";
 document.getElementById('upaside2').style.display="inline-block";
 document.getElementById('topnav').style.height='165px';
@@ -6,5 +7,6 @@ document.getElementById('topnav').style.height='165px';
 document.getElementById('respbar1').addEventListener('click', function() {
     document.getElementById('upaside2').style.display="none";
     document.getElementById('upaside3').style.display="inline-block";
-    document.getElementById('topnav').style.height='40px';
+    document.getElementById('topnav').style.height='34px';
+    document.getElementById('upaside1').style.padding="7px 8px";
 });
